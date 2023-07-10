3a8af7eca87ddf2acb6c9ee20b14d6946d801459
Taman Gill <tgill0424@gmail.com>
2023-07-09 21:21:53 -0700

    firstcommit

---
+++ src/components/home.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+
+
+const Home = () => {
+  return (
+    <div className="container">
+      <h1 className="title">Welcome to Itinerary Generator</h1>
+      <Carousel>
+        <div>
+          <img src="https://example.com/image1.jpg" alt="Image 1" />
+        </div>
+        <div>
+          <img src="https://example.com/image2.jpg" alt="Image 2" />
+        </div>
+        <div>
+          <img src="https://example.com/image3.jpg" alt="Image 3" />
+        </div>
+      </Carousel>
+    </div>
+  );
+};
+
+export default Home;
