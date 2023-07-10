aef08344af38d27868487094a0deea7163b98fd6
Taman Gill <tgill0424@gmail.com>
2023-07-10 11:12:46 -0700

    pictures added

--- src/components/home.js
+++ src/components/home.js
@@ -9,13 +9,16 @@ const Home = () => {
       <h1 className="title">Welcome to Itinerary Generator</h1>
       <Carousel>
         <div> 
-          <img src="https://example.com/image1.jpg" alt="Image 1" />
+          <img src="https://www.bbb.org/all/travel-leisure/_jcr_content/socialImage.coreimg.jpeg/1624972217250/gettyimages-155375632.jpeg" alt="Image 1" />
         </div>
         <div>
-          <img src="https://example.com/image2.jpg" alt="Image 2" />
+          <img src="https://cdn.pixabay.com/photo/2016/03/04/19/36/beach-1236581_1280.jpg" alt="Image 2" />
         </div>
         <div>
-          <img src="https://example.com/image3.jpg" alt="Image 3" />
+          <img src="https://images.pexels.com/photos/1371360/pexels-photo-1371360.jpeg?cs=srgb&dl=pexels-te-lensfix-1371360.jpg&fm=jpg" alt="Image 3" />
+        </div>
+        <div>
+          <img src="https://images.pexels.com/photos/3278215/pexels-photo-3278215.jpeg?cs=srgb&dl=pexels-oleksandr-canary-islands-3278215.jpg&fm=jpg" alt="Image 4" />
         </div>
       </Carousel>
     </div>
